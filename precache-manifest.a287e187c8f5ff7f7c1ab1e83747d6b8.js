2f13806d1ff398b681fe7dd768f2cde942ded388
Diego Paguay <gosuetian@hotmail.es>
2020-05-26 21:32:06 -0500

    Initial commit

---
+++ precache-manifest.a287e187c8f5ff7f7c1ab1e83747d6b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73355051b6954a7767fbd9cd01a73dba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18b337b55bc58eedfe2c",
+    "url": "./static/js/2.1ed62b1f.chunk.js"
+  },
+  {
+    "revision": "9d2f9bca43504fa2ca48737f8bc6935b",
+    "url": "./static/js/2.1ed62b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a7c49cd8a23d7fa014",
+    "url": "./static/js/main.62394daf.chunk.js"
+  },
+  {
+    "revision": "0480b89ac5ab3143f61a",
+    "url": "./static/js/runtime-main.59f975ab.js"
+  }
+]);
